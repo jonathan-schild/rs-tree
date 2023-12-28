4824896d9ef8509551f261427601fab2bec0bf1d
Jonathan "Nath" Schild <nath@j-schild.de>
2023-12-28 21:59:32 +0100

    Add redis session store

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use actix_web::{App, HttpServer};
+use actix_session::{storage::RedisSessionStore, SessionMiddleware};
+use actix_web::{cookie::Key, App, HttpServer};
 
 mod url_management;
 
+fn get_secret_key() -> Key {
+    Key::generate()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(url_management::config))
-        .bind(("127.0.0.1", 8080))?
-        .run()
+    let secret_key = get_secret_key();
+    let redis_connection_string = "redis://127.0.0.1:6379";
+    let store = RedisSessionStore::new(redis_connection_string)
         .await
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(SessionMiddleware::new(store.clone(), secret_key.clone()))
+            .configure(url_management::config)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
