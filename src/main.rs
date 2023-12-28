9d284104a72ed867222dbce064f28cab13055ed5
Jonathan "Nath" Schild <nath@j-schild.de>
2023-12-28 21:29:32 +0100

    Add base api routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+mod url_management;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().configure(url_management::config))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
---
+++ src/url_management.rs
@@ -0,0 +1,32 @@
+use actix_web::{
+    delete, get, post,
+    web::{Path, ServiceConfig},
+    HttpResponse, Responder,
+};
+
+pub fn config(cfg: &mut ServiceConfig) {
+    cfg.service(resolve)
+        .service(create)
+        .service(create_anon)
+        .service(delete);
+}
+
+#[get("/resolve/{id}")]
+async fn resolve(_id: Path<String>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[post("/create/{id}")]
+async fn create(_id: Path<String>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[post("/create")]
+async fn create_anon() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[delete("/delete/{id}")]
+async fn delete(_id: Path<String>) -> impl Responder {
+    HttpResponse::Ok()
+}
