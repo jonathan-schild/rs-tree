f79b26971bc758cbb3bc7cfff18a4bc501072a0a
Jonathan "Nath" Schild <nath@j-schild.de>
2024-01-06 00:35:47 +0100

    Add seed bin

---
+++ src/bin/seed.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+use rs_tree::utility::hash_password;
+use uuid::Uuid;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// generates new v4 uuid
+    Uuid {
+        /// generates a all zero uuid
+        #[arg(short, long, default_value = "false")]
+        nil: bool,
+    },
+    /// creates a hash of `password`
+    Hash { password: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Uuid { nil } => {
+            if nil {
+                println!("{}", Uuid::nil())
+            } else {
+                println!("{}", Uuid::new_v4())
+            }
+        }
+        Commands::Hash { password } => println!("{}", hash_password(&password)),
+    }
+}
